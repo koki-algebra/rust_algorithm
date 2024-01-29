780c58db9df29add3f535796fbe78cdd18a824ea
koki-algebra <yotsuba00040@gmail.com>
2024-01-29 14:34:10 +0900

    typical90 007

---
+++ atcoder/typical90/examples/007/main.rs
@@ -0,0 +1,31 @@
+use std::cmp::min;
+
+use proconio::input;
+
+fn main() {
+    input! {
+        n: i32,
+        mut a: [i32; n],
+        q: i32,
+        b: [i32; q],
+    }
+
+    a.sort();
+
+    for v in &b {
+        // binary search
+        let index = match a.binary_search(v) {
+            Ok(index) => index,
+            Err(index) => index,
+        };
+
+        let mut ans = u32::MAX;
+        if index != a.len() {
+            ans = min(ans, v.abs_diff(a[index]));
+        }
+        if index >= 1 {
+            ans = min(ans, v.abs_diff(a[index - 1]));
+        }
+        println!("{ans}");
+    }
+}
