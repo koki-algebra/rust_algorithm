74a19fca694b47f4731190b8f6d28281c287da8f
koki-algebra <yotsuba00040@gmail.com>
2024-02-19 18:26:57 +0900

    typical90 033

---
+++ atcoder/typical90/examples/033/main.rs
@@ -0,0 +1,14 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        h: usize,
+        w: usize,
+    }
+
+    if h == 1 || w == 1 {
+        println!("{}", h * w);
+    } else {
+        println!("{}", ((h + 1) / 2) * ((w + 1) / 2));
+    }
+}
