28464ad948dc2579c8a0a4e57bb53f1a84886b94
koki-algebra <yotsuba00040@gmail.com>
2024-02-29 18:00:00 +0900

    typical90 061

---
+++ atcoder/typical90/examples/061/main.rs
@@ -0,0 +1,21 @@
+use std::collections::VecDeque;
+
+use proconio::input;
+
+fn main() {
+    input! {
+        q: usize,
+        queries: [(usize, usize); q],
+    }
+
+    let mut deque = VecDeque::new();
+    for &(t, x) in queries.iter() {
+        if t == 1 {
+            deque.push_front(x);
+        } else if t == 2 {
+            deque.push_back(x);
+        } else {
+            println!("{}", deque[x - 1]);
+        }
+    }
+}
