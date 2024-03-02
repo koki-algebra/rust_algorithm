e4f3e68cb73ba0628da2afc2816c0187f3899f82
koki-algebra <yotsuba00040@gmail.com>
2024-03-02 14:40:50 +0900

    fix typical90 063

--- atcoder/typical90/examples/063/main.rs
+++ atcoder/typical90/examples/063/main.rs
@@ -15,16 +15,15 @@ fn main() {
         // key: number, value: count
         let mut m = HashMap::new();
 
-        for j in 0..w {
+        for col in 0..w {
             let mut num = -1;
             let mut valid = true;
-
-            for i in 0..h {
-                if (bit >> i & 1) == 1 {
+            for row in 0..h {
+                if bit >> row & 1 == 1 {
                     if num == -1 {
-                        num = p[i][j];
-                    } else if num != p[i][j] {
-                        valid = false
+                        num = p[row][col];
+                    } else if p[row][col] != num {
+                        valid = false;
                     }
                 }
             }
@@ -34,16 +33,16 @@ fn main() {
             }
         }
 
+        // number of columns
+        let cnt_w = maximum_same(m);
+
         // number of rows
         let mut cnt_h = 0;
-        for i in 0..h {
-            if (bit >> i & 1) == 1 {
+        for row in 0..h {
+            if bit >> row & 1 == 1 {
                 cnt_h += 1;
             }
         }
-
-        // number of columns
-        let cnt_w = maximum_same(m);
 
         chmax(&mut ans, cnt_h * cnt_w);
     }
@@ -53,7 +52,7 @@ fn main() {
 
 fn maximum_same(m: HashMap<isize, isize>) -> isize {
     let mut ret = -1;
-    for (_, &value) in m.iter() {
+    for (_key, &value) in m.iter() {
         chmax(&mut ret, value);
     }
 
