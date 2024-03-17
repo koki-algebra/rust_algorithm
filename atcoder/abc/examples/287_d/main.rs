4a41ac6e269baa1b00ce6fe977683917ea6be603
koki-algebra <yotsuba00040@gmail.com>
2024-03-17 23:27:19 +0900

    ABC287 D

--- atcoder/abc/examples/287_d/main.rs
+++ atcoder/abc/examples/287_d/main.rs
@@ -7,18 +7,16 @@ fn main() {
         t: Chars,
     }
 
-    // pre[i]: S の先頭 i 文字が T の先頭 i 文字と一致するかどうか
+    // pre[i]: S と T の先頭 i 文字と一致するかどうか
     let mut pre = vec![false; t.len() + 1];
     pre[0] = true;
-    // suf[i]: T の末尾　 i 文字と T の末尾 i 文字が一致するかどうか
+    // suf[i]: S と T の末尾 i 文字が一致するかどうか
     let mut suf = vec![false; t.len() + 1];
     suf[0] = true;
 
     for i in 1..=t.len() {
-        pre[i] = (s[i - 1] == t[i - 1] || t[i - 1] == '?' || s[i - 1] == '?') && pre[i - 1];
-        suf[i] =
-            (t[t.len() - i] == s[s.len() - i] || t[t.len() - i] == '?' || s[s.len() - i] == '?')
-                && suf[i - 1];
+        pre[i] = is_match(s[i - 1], t[i - 1]) && pre[i - 1];
+        suf[i] = is_match(s[s.len() - i], t[t.len() - i]) && suf[i - 1];
     }
 
     for x in 0..=t.len() {
@@ -28,4 +26,8 @@ fn main() {
             println!("No")
         }
     }
+}
+
+fn is_match(c1: char, c2: char) -> bool {
+    return c1 == c2 || c1 == '?' || c2 == '?';
 }
