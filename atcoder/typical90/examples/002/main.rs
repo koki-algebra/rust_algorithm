a2a2ed38f7fa89fe7a6636ad6e75ae314d60da4e
koki-algebra <yotsuba00040@gmail.com>
2024-03-26 00:09:45 +0900

    typical90 002

--- atcoder/typical90/examples/002/main.rs
+++ atcoder/typical90/examples/002/main.rs
@@ -3,29 +3,26 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        n: i32
+        n: usize,
     }
 
     for bit in 0..1 << n {
-        let mut s = String::from("");
+        let mut s = String::new();
         let mut cnt = 0;
-
         for i in 0..n {
             if bit >> i & 1 == 1 {
-                s = format!("{}{}", ")", s);
+                s = format!("){}", s);
                 cnt += 1;
             } else {
-                s = format!("{}{}", "(", s);
+                s = format!("({}", s);
                 cnt -= 1;
             }
-
             if cnt < 0 {
                 break;
             }
         }
-
         if cnt == 0 {
-            println!("{s}");
+            println!("{}", s);
         }
     }
 }
