eca2305703cd544b420ad1207dd3f8b38434edeb
koki-algebra <yotsuba00040@gmail.com>
2024-04-04 14:18:45 +0900

    typical90 046

--- atcoder/typical90/examples/046/main.rs
+++ atcoder/typical90/examples/046/main.rs
@@ -1,7 +1,8 @@
-use proconio::input;
+use proconio::{fastout, input};
 
 const MOD: usize = 46;
 
+#[fastout]
 fn main() {
     input! {
         n: usize,
@@ -10,26 +11,21 @@ fn main() {
         c: [usize; n],
     }
 
-    let mut m_a: Vec<isize> = vec![0; MOD];
-    let mut m_b: Vec<isize> = vec![0; MOD];
-    let mut m_c: Vec<isize> = vec![0; MOD];
-
-    for &v in a.iter() {
-        m_a[v % MOD] += 1;
-    }
-    for &v in b.iter() {
-        m_b[v % MOD] += 1;
-    }
-    for &v in c.iter() {
-        m_c[v % MOD] += 1;
+    let mut a_mod: Vec<usize> = vec![0; MOD];
+    let mut b_mod: Vec<usize> = vec![0; MOD];
+    let mut c_mod: Vec<usize> = vec![0; MOD];
+    for i in 0..n {
+        a_mod[a[i] % MOD] += 1;
+        b_mod[b[i] % MOD] += 1;
+        c_mod[c[i] % MOD] += 1;
     }
 
     let mut ans = 0;
-    for x in 0..MOD {
-        for y in 0..MOD {
-            for z in 0..MOD {
-                if (x + y + z) % MOD == 0 {
-                    ans += m_a[x] * m_b[y] * m_c[z];
+    for i in 0..MOD {
+        for j in 0..MOD {
+            for k in 0..MOD {
+                if (i + j + k) % MOD == 0 {
+                    ans += a_mod[i] * b_mod[j] * c_mod[k];
                 }
             }
         }
