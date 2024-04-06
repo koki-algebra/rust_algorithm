407c2f8d977739f0b254ddca0cff983cb6c92963
koki-algebra <yotsuba00040@gmail.com>
2024-04-06 14:47:29 +0900

    typical90 069

--- atcoder/typical90/examples/069/main.rs
+++ atcoder/typical90/examples/069/main.rs
@@ -27,31 +27,15 @@ fn main() {
 fn pow(base: usize, exp: usize, m: usize) -> usize {
     let mut ret = 1;
     let mut x = base;
-    let digits = count_binary_digits(exp);
 
-    for i in 0..digits {
-        if exp >> i & 1 == 1 {
+    let mut num = exp;
+    while num > 0 {
+        if num & 1 == 1 {
             ret = ret * x % m;
         }
-
         x = x * x % m;
+        num >>= 1;
     }
 
     ret
-}
-
-// Calculate the number of digits when representing a decimal integer in binary. O(log(n))
-fn count_binary_digits(n: usize) -> usize {
-    if n == 0 {
-        return 1;
-    }
-    let mut cnt = 0;
-    let mut num = n;
-
-    while num > 0 {
-        num >>= 1;
-        cnt += 1;
-    }
-
-    cnt
 }
