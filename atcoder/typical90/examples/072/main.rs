72d5a1c96b5ccd051c57c2553ed0b2ed7bb79bba
koki-algebra <yotsuba00040@gmail.com>
2024-04-13 14:05:36 +0900

    typical90 072

--- atcoder/typical90/examples/072/main.rs
+++ atcoder/typical90/examples/072/main.rs
@@ -5,56 +5,64 @@ fn main() {
     input! {
         h: usize,
         w: usize,
-        c: [Chars; h],
+        field: [Chars; h],
     }
 
     let mut seen = vec![vec![false; w]; h];
-    let mut ans = -1;
 
-    for i in 0..h {
-        for j in 0..w {
-            chmax(&mut ans, dfs(&mut seen, &c, i, j, i, j));
+    let mut ans = 0;
+    for row in 0..h {
+        for col in 0..w {
+            chmax(&mut ans, dfs(&field, &mut seen, row, col, row, col));
         }
     }
 
     if ans <= 2 {
-        ans = -1;
+        println!("{}", -1);
+        return;
     }
 
     println!("{}", ans);
 }
 
 fn dfs(
+    field: &Vec<Vec<char>>,
     seen: &mut Vec<Vec<bool>>,
-    c: &Vec<Vec<char>>,
     s_row: usize,
     s_col: usize,
     p_row: usize,
     p_col: usize,
 ) -> isize {
-    if s_row == p_row && s_col == p_col && seen[p_row][p_col] {
+    if p_row == s_row && p_col == s_col && seen[p_row][p_col] {
         return 0;
     }
 
-    let h = c.len();
-    let w = c[0].len();
-
-    let d_row: [i32; 4] = [0, 1, 0, -1];
-    let d_col: [i32; 4] = [1, 0, -1, 0];
-
     seen[p_row][p_col] = true;
+
+    let h = field.len();
+    let w = field[0].len();
+
+    let dr = [-1, 0, 1, 0];
+    let dc = [0, 1, 0, -1];
+
     let mut ret = -isize::MAX;
 
-    for i in 0..4 {
-        let next_row = (p_row as i32 + d_row[i]) as usize;
-        let next_col = (p_col as i32 + d_col[i]) as usize;
+    for dir in 0..4 {
+        let next_row = p_row as isize + dr[dir];
+        let next_col = p_col as isize + dc[dir];
+        if next_row < 0 || next_col < 0 {
+            continue;
+        }
 
-        if next_row < h && next_col < w && c[next_row][next_col] == '.' {
-            if (next_row != s_row || next_col != s_col) && seen[next_row][next_col] {
+        let next_row = next_row as usize;
+        let next_col = next_col as usize;
+
+        if next_row < h && next_col < w && field[next_row][next_col] == '.' {
+            if !(next_row == s_row && next_col == s_col) && seen[next_row][next_col] {
                 continue;
             }
 
-            let v = dfs(seen, c, s_row, s_col, next_row, next_col);
+            let v = dfs(field, seen, s_row, s_col, next_row, next_col);
             chmax(&mut ret, v + 1);
         }
     }
@@ -67,8 +75,8 @@ fn dfs(
 fn chmax(a: &mut isize, b: isize) -> bool {
     if *a < b {
         *a = b;
-        true
-    } else {
-        false
+        return true;
     }
+
+    false
 }
