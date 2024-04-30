8a00e140f247ea111ce01852050caef15d4e1ff5
Yoshimi-I <89241539+yoshimi-I@users.noreply.github.com>
2024-05-01 01:16:17 +0900

    Update binary_search.rs
    コメント消したンゴ

--- algorithm/src/binary_search.rs
+++ algorithm/src/binary_search.rs
@@ -2,7 +2,6 @@ use std::cmp::Ordering;
 
 pub fn binary_search<T: Ord>(item: T, arr: &[T]) -> Option<usize> {
     let mut is_asc = true;
-    // これはコメントです
     if arr.len() > 1 {
         is_asc = arr[0] < arr[arr.len() - 1];
     }
